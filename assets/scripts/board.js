bda698d59577de1afd019195b186dc6a150bcedf
diegopumacode <diegopumacode@gmail.com>
2021-02-11 18:27:31 -0500

    adding myboards and closed functionality

--- assets/scripts/board.js
+++ assets/scripts/board.js
@@ -1,3 +1,5 @@
+import Boards from "./boards.js";
+import ClosedBoards from "./closedBoards.js";
 
 
 export default function Board(parentSelector) {
@@ -14,12 +16,12 @@ export default function Board(parentSelector) {
     <main>
         <section class="container">
             <nav class="container--navbar">
-                <a href=" ">My boards</a>
-                <a href=" ">Closed boards</a>
+                <a class="js-select-myboards" href="#">My boards</a>
+                <a class="js-select-closedBoards" href="#">Closed boards</a>
                 <a href=" "> My Profile</a>
                 <button class="container--navbar__logout">Log out</button>
             </nav>
-            <article class="container--options">
+            <article class="container--options js-container-options">
                 <p>Boards</p>
             </article>
         </section>
@@ -33,6 +35,35 @@ export default function Board(parentSelector) {
 
 Board.prototype.render = function () {
   this.parentElement.innerHTML = this;
-
+  this.myBoards()
+  this.myBoardsListener()
+  this.closedBoardsListener()
 };
+
+Board.prototype.myBoards = function () {
+  this.parentElement.innerHTML = this;
+  const boards = new  Boards('.container--options')
+  boards.render()
+};
+
+Board.prototype.myBoardsListener = function () {
+  const myBoardAction= document.querySelector('.js-select-myboards')
+  myBoardAction.addEventListener("click",(e)=>{
+      console.log("select boards ")
+      e.preventDefault()
+      const boards = new Boards()
+      boards.render()
+    })
+};
+Board.prototype.closedBoardsListener = function () {
+  const myBoardAction= document.querySelector('.js-select-closedBoards')
+  myBoardAction.addEventListener("click",(e)=>{
+      console.log("slect closed boards")
+      e.preventDefault()
+      const closedBoards = new ClosedBoards('.container--options')
+      closedBoards.render()
+  })
+};
+
+
 

70647ae7d63893c2e2a57585446cce56c0dc8824
Cesar Cachay <cesarcachay1996@gmail.com>
2021-02-11 17:12:05 -0500

    change branch

--- assets/scripts/services/userService.js
+++ assets/scripts/services/userService.js
@@ -7,4 +7,35 @@ function UserService() {
   return UserService.instance;
   }
 
+UserService.prototype.register = (username, email,first_name,last_name,password) =>
+
+  apiFetch(`${BASE_URL}/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", 
+    },
+    body: JSON.stringify({ username, email,first_name, last_name,password}),
+  });
+
+
+UserService.prototype.update = (id, username, email,first_name,last_name,password) =>
+
+  apiFetch(`${BASE_URL}/users/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token toke=${sessionStorage.getItem("token")}`
+    },
+    body: JSON.stringify({ username, email,first_name, last_name, password}),
+  });
+
+UserService.prototype.destroy = (id) =>
+
+  apiFetch(`${BASE_URL}/users/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Token toke=${sessionStorage.getItem("token")}`
+    },
+  });
+
 export default UserService; 

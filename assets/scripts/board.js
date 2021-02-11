9cfac2c61ce6a0f8cd2f8cf13132574b7734b018
bllerenac <bllerenac@uni.pe>
2021-02-11 11:59:59 -0500

    render board move board.js

--- assets/scripts/board.js
+++ assets/scripts/board.js
@@ -6,9 +6,24 @@ export default function Board(parentSelector) {
     this.parentElement = document.querySelector(parentSelector);
     this.toString = function () {
       return `
-      <section>
-        <h2>Board</h2>
-      </section>
+      <header>
+        <div class="organizable">
+            {organiz<span class="organizable__able">able</span>}
+        </div>
+    </header>
+    <main>
+        <section class="container">
+            <nav class="container--navbar">
+                <a href=" ">My boards</a>
+                <a href=" ">Closed boards</a>
+                <a href=" "> My Profile</a>
+                <button class="container--navbar__logout">Log out</button>
+            </nav>
+            <article class="container--options">
+                <p>Boards</p>
+            </article>
+        </section>
+    </main>
       `;
     };
     Board.instance = this;

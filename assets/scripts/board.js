9e7b58b3dc0440090824b67db807d5a2404910ab
Cesar Cachay <cesarcachay1996@gmail.com>
2021-02-11 17:08:45 -0500

    update main

--- assets/scripts/board.js
+++ assets/scripts/board.js
@@ -1,29 +1,20 @@
-
-
 export default function Board(parentSelector) {
   if (!Board.instance) {
     this.parentSelector = parentSelector;
     this.parentElement = document.querySelector(parentSelector);
     this.toString = function () {
       return `
-      <header>
-        <div class="organizable">
-            {organiz<span class="organizable__able">able</span>}
-        </div>
-    </header>
-    <main>
-        <section class="container">
-            <nav class="container--navbar">
-                <a href=" ">My boards</a>
-                <a href=" ">Closed boards</a>
-                <a href=" "> My Profile</a>
-                <button class="container--navbar__logout">Log out</button>
-            </nav>
-            <article class="container--options">
-                <p>Boards</p>
-            </article>
-        </section>
-    </main>
+      <section class="container">
+        <nav class="container--navbar">
+          <a href=" ">My boards</a>
+          <a href=" ">Closed boards</a>
+          <a href=" "> My Profile</a>
+          <button class="container--navbar__logout">Log out</button>
+        </nav>
+        <article class="container--options">
+           <p>Boards</p>
+        </article>
+      </section>
       `;
     };
     Board.instance = this;
@@ -33,6 +24,4 @@ export default function Board(parentSelector) {
 
 Board.prototype.render = function () {
   this.parentElement.innerHTML = this;
-
 };
-

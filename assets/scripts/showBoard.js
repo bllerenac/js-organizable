cf5c95c9048f00e71568142a626ee0452616f00a
bllerenac <bllerenac@uni.pe>
2021-02-13 13:55:26 -0500

    list render complete

--- assets/scripts/showBoard.js
+++ assets/scripts/showBoard.js
@@ -1,35 +1,48 @@
 import STORE from "./store.js";
+import List from "./show_board/lists.js"
 
-export default class ShowBoard{
-
-  constructor(parentSelector){
+export default function ShowBoard(parentSelector){
+  if (!ShowBoard.instance) {
     this.parentSelector = parentSelector;
-  }
-
-  toString(){
+    this.parentElement = document.querySelector(parentSelector);
     let {name,color} = STORE.boardSelected
-    return `
+    this.toString = function () {
+      return `
       <section class="show-board ${color}">
-        <div class="show-board__header">
-          <h3>${name}</h3>
+      <div class="show-board__header">
+        <h3>${name}</h3>
 
-          <button class="btn-header-show">
-            <img src="./assets/images/star.svg" />
-          </button>
-          <button class="btn-header-show">
-            <img src="./assets/images/closed.svg" />
-          </button>
-        </div>
+        <button class="btn-header-show">
+          <img src="./assets/images/star.svg" />
+        </button>
+        <button class="btn-header-show">
+          <img src="./assets/images/closed.svg" />
+        </button>
+      </div>
 
-        <div class="show-board__content">
-          Aqui pones tu contenio
-        </div>
-      </section>
-      `;
+      <div class="show-board__content">
+        Aqui pones tu contenio
+      </div>
+    </section>
+        `;
+    };
+    ShowBoard.instance = this;
   }
+  return ShowBoard.instance;
+};
+  
+ShowBoard.prototype.render = function () {
+   this.parentElement.innerHTML = this;
+   this.renderList();
+};
 
-  render(){
-    document.querySelector(this.parentSelector).innerHTML = this.toString()
-  }
+ShowBoard.prototype.renderList = function () {
+  const data = STORE.boardSelected.lists;
+  const board__content = document.querySelector(".show-board__content");
+  const list_render = data.map((list_data) => {
+    console.log(list_data)
+    return new List(".show-board__content",list_data);
+  });
 
+  return board__content.innerHTML = list_render.join("") 
 }

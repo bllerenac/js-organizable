dccc6c8b98c4da7b8084cedf85584285ecf87723
diegopumacode <diegopumacode@gmail.com>
2021-02-14 19:32:30 -0500

    adding modal show board

---
+++ assets/scripts/show_board/modal.js
@@ -0,0 +1,37 @@
+import STORE from "../store.js"
+import Label from "./card_label.js"
+
+export default function Modal(cardData) {
+
+    this.data = cardData
+    this.toString = function () {
+        console.log(this.data)
+        return `
+        <div class="modal">
+          <div class="modal__header">
+                <p>${this.data.name}</p>
+                <button class="js-close-modal close-modal">X</button>
+          </div>
+        </div>
+        `;
+    };
+}
+
+
+Modal.prototype.render = function () {
+    let contentModal = document.createElement("DIV")
+    contentModal.classList.add("content-modal")
+    contentModal.innerHTML = this
+    document.body.appendChild(contentModal)
+    this.close()
+}
+
+
+Modal.prototype.close = function () {
+    const closeBtn = document.querySelector(".js-close-modal")
+    closeBtn.addEventListener("click",(e)=>{
+        const cModal = document.querySelector(".content-modal")
+        cModal.remove()
+
+    })
+}
